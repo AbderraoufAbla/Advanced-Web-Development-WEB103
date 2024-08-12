035e59b53635dbd88b11eeb6062d38240a7c6128
Abderraouf Ahmed Abla <94922753+AbderraoufAbla@users.noreply.github.com>
2024-08-12 12:58:23 +0100

    Update CreatorCard.jsx

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CreatorCard({ creator }) {
+  return (
+    <div className="card">
+      <img src={creator.imageURL} alt={creator.name} />
+      <h3>{creator.name}</h3>
+      <p>{creator.description}</p>
+      <a href={creator.url} target="_blank" rel="noopener noreferrer">Visit</a>
+      <Link to={`/edit/${creator.id}`}>Edit</Link>
+    </div>
+  );
+}
+
+export default CreatorCard;
 
