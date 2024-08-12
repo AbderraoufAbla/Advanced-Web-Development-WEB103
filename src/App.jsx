47396de36ef76d516f3b404325be5f9703ab288f
Abderraouf Ahmed Abla <94922753+AbderraoufAbla@users.noreply.github.com>
2024-08-12 13:04:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ShowCreators from './pages/ShowCreators';
+import AddCreator from './pages/AddCreator';
+import EditCreator from './pages/EditCreator';
+import ViewCreator from './pages/ViewCreator';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ShowCreators />} />
+        <Route path="/add" element={<AddCreator />} />
+        <Route path="/edit/:id" element={<EditCreator />} />
+        <Route path="/view/:id" element={<ViewCreator />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
