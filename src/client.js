e2e5915d3dfec0f85ba7ca79b88ef1b9abae627e
Abderraouf Ahmed Abla <94922753+AbderraoufAbla@users.noreply.github.com>
2024-08-12 12:57:47 +0100

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'YOUR_SUPABASE_URL';
+const API_KEY = 'YOUR_SUPABASE_API_KEY';
+export const supabase = createClient(URL, API_KEY);
 
